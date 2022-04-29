fbf04389a4e8314f17066c6cfccc55ffe6eeb6b6
STV <ecalcinap@unsa.edu.pe>
2022-04-29 09:26:35 -0500

    se agrego funcion multiplicar

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,7 @@
 class Calculator {
   int add(int a, int b){ return a+b; }
   int sub(int a, int b){ return a-b; }
-  int mul(int a, int b){ return 0; }
+  int mul(int a, int b){ return a*b; }
   int div(int a, int b){ return 0; }
   int mod(int a, int b){ return 0; }
 }
